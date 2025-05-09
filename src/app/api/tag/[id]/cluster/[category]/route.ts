54926667e52dd77c490ec9cd2c392432ec3b13e7
CaptainCrouton89 <rhyneer.silas@gmail.com>
2025-05-09 04:15:09 -0700

    feat: increased max function call

--- src/app/api/tag/[id]/cluster/[category]/route.ts
+++ src/app/api/tag/[id]/cluster/[category]/route.ts
@@ -2,6 +2,8 @@ import { initializeServices } from "@/lib/services";
 import { Category } from "@/types/types";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 60;
+
 export const POST = async (
   request: Request,
   { params }: { params: Promise<{ id: number; category: Category }> }
